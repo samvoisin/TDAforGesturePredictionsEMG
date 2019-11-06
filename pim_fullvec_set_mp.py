e1d48240e227512d981eacb4197e7ce008bdc38a
Sam Voisin <psv6@duke.edu>
2019-11-05 22:24:57 -0500

    full vec ds par

--- pim_fullvec_set_mp.py
+++ pim_fullvec_set_mp.py
@@ -18,29 +18,6 @@ from TDA_helper_fcns import load_data, sublevel_set_time_series_dist
 
 ########################## parallel helper functions ###########################
 
-
-def gest_to_pim(garray, px, sd):
-    """
-    Convert a gesture to a persistence image. The garray input should not
-    include time series, subject number, gesture number columns.
-
-    INPUTS
-    garray - gesture array w/o time series & w/o subject and gesture number
-    px - pixel dimension/ resolution; e.g. px=20 gives 20x20 persistence image
-    sd - persistence image concentration parameter (gaussian)
-
-    OUTPUTS
-    flattened persistence image vector of length px**2
-    """
-    # instantiate persistence image generator & vietoris-rips complex generators
-    rips = Rips(maxdim=1, verbose=False) # 1-D homology rips complex
-    pim = PersImage(pixels=[px,px], spread=sd)
-
-    dgms = rips.fit_transform(garray) # generate rips complex on points
-    img = pim.transform(dgms[1]) # persistence image of 1 cycles
-    return img.flatten()
-
-
 def subj_to_pims(sbj, sdict, px, sd):
     """
     generate persistence images for all gestures for a given subject
@@ -51,19 +28,31 @@ def subj_to_pims(sbj, sdict, px, sd):
     sd - persistence image concentration parameter (gaussian)
 
     OUTPUTS
+    array of gestures made by subject
     """
+    # instantiate persistence image generator & vietoris-rips complex generators
+    rips = Rips(maxdim=1, verbose=False) # 1-D homology rips complex
+    pim = PersImage(pixels=[px,px], spread=sd)
+    nobs = 24 # each subject performs 24 gestures
+    # each vector have equal # persim pix + 2 cols for subj & gest labels
+    res_mat = np.zeros(px**2*nobs + 2*nobs).reshape(nobs, -1)
+
+    v = 0
     for gnum, garray in sdict.items():
-        # flattened pim vector, gesture number, subject number
-        return np.r_[
-            gest_to_pim(garray[:,1:-2], pim_px, pim_sd),int(gnum[0]),int(sbj)
-            ]
+        # generate rips complex on points; slice out time col and gesture label
+        dgms = rips.fit_transform(garray[:, 1:-1])
+        img = pim.transform(dgms[1]) # persistence image of 1 cycles
+        obs_vec = np.r_[img.flatten(), int(gnum[0]), int(sbj)]
+        res_mat[v, :] = obs_vec
+        v += 1
+
+    return res_mat
 
 ################################################################################
-# https://www.machinelearningplus.com/python/parallel-processing-python/
 
 if __name__ == "__main__":
 
-    gdat = load_data(subjects=["01", "21"]) # gestures data (test w/ 2)
+    gdat = load_data(subjects=["01", "03"]) # gestures data (test w/ 2)
     nvects = len(gdat.keys()) * 24 # each subject performs 24 total gestures
     pim_px = 20 # persistence image dims (square)
     pim_sd = 1e-5 # persistence image st. dev.
@@ -72,22 +61,26 @@ if __name__ == "__main__":
     matsize = pim_px**2*nvects + 2*nvects
     pim_mat = np.zeros(matsize).reshape(nvects, -1) # init array for pim vects
 
-    pool = mp.Pool(4) # use 4 CPU cores
+    pool = mp.Pool(4) # specify number of CPU cores
 
-
-    ##### here down has problems #############
-
-    par_res = [pool.apply_async(
-        subj_to_pims,
-        args=(sbj, sdict, pim_px, pim_sd)
-            ) for sbj, sdict in gdat.items()]
+    par_res = [
+    pool.apply_async(subj_to_pims, args=(sbj, sdict, pim_px, pim_sd)
+    ) for sbj, sdict in gdat.items()
+    ]
 
     pool.close()
+    pool.join()
 
+    # stack persistence image vectors
+    r = 0
+    for i in par_res:
+        pim_mat[r:r+24, :] = i.get()
+        r += 24
 
+    print(pim_mat)
     # save matrix as DataFrame
     pim_df = pd.DataFrame(pim_mat)
     cnames = ["px"+str(i) for i in pim_df.columns]
     cnames[-2:] = ["gest", "subj"]
     pim_df.columns = cnames
-    pim_df.to_csv("./Data/pim_vectors.csv", index=False)
+    pim_df.to_csv("./Data/pim_vectors_mp.csv", index=False)
---
+++ pim_git.sh
@@ -0,0 +1,4 @@
+python3 pim_fullvec_set_pm.py
+git add -A
+git commit -m "full vec ds par"
+git push
