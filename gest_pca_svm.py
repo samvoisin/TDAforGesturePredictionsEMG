35863213030514d8e94448c35248151ee2421ff0
Sam Voisin <psv6@duke.edu>
2019-11-21 16:14:50 -0500

    pca stuff

--- gest_pca_svm.py
+++ gest_pca_svm.py
@@ -30,7 +30,7 @@ evals = evals.real[eidx]
 
 # skree plot
 skree = [sum(evals[:n+1]) for n, i in enumerate(evals)]
-sns.scatterplot(range(10), skree[:10])
+sns.scatterplot(range(1, 11), skree[:10])
 plt.show()
 
 ########
@@ -45,8 +45,8 @@ pca_df = pd.DataFrame(np.c_[PCApims, pim_df.values[:, -2]])
 pca_df.columns = ["V1", "V2", "gest"]
 pca_df.gest = pca_df.gest.astype("int32")
 
-sns.scatterplot("V1", "V2", hue="gest", data=pca_df)
-plt.show()
+#sns.scatterplot("V1", "V2", hue="gest", data=pca_df)
+#plt.show()
 
 eigbase = evecs[:, eval:eval+3]
 PCApims = pims @ eigbase
