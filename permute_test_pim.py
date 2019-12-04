153c597cc470e82ba2a20f6f47ea01c8220d5f8b
Sam Voisin <psv6@duke.edu>
2019-12-04 13:50:01 -0500

    added permutation code

--- permute_test_pim.py
+++ permute_test_pim.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
-import pickle
+
+from numpy.random import choice
 
 from persim import PersImage
 
@@ -29,15 +30,40 @@ log_reg = LogisticRegression(
     C=1e6,
     solver="newton-cg",
     fit_intercept=True,
-    max_iter=5000,
+    max_iter=1500,
     multi_class="multinomial",
     random_state=1)
 
-skf = StratifiedKFold(n_splits = 5, random_state = 1, shuffle = True)
+s = 5 # number of CV folds
+skf = StratifiedKFold(n_splits = s, random_state = 1, shuffle = True)
 
-fold_acc = []
+fold_acc = np.zeros((1, s))
+f = 0
 for train_idx, test_idx in skf.split(pims, gests):
+    print("Fold number " + str(f), end="\r")
     log_reg.fit(pims[train_idx, ], gests[train_idx])
-    fold_acc.append(log_reg.score(pims[test_idx, ], gests[test_idx]))
+    fold_acc_perm[1, f] = log_reg.score(pims[test_idx, ], gests[test_idx])
+    f += 1
 
-print(fold_acc)
+
+########## now compute accuracy score with randomly permuted labels ############
+perm_gests = gests[choice(
+    np.arange(gests.size),
+    size = gests.size,
+    replace = False)
+    ]
+
+fold_acc_perm = np.zeros((1, s))
+f = 0
+for train_idx, test_idx in skf.split(pims, perm_gests):
+    print("Fold number " + str(f), end="\r")
+    log_reg.fit(pims[train_idx, ], perm_gests[train_idx])
+    fold_acc_perm[1, f] = log_reg.score(pims[test_idx, ], perm_gests[test_idx])
+    f += 1
+
+
+scores = np.r_[fold_acc, fold_acc_perm]
+print(scores)
+
+print("Avg Accuracy:")
+print(scores.mean(axis=1))
