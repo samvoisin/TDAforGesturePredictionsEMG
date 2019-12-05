5b0bef163389576e33f1ad64d3b2b8e83ef98ce0
Sam Voisin <psv6@duke.edu>
2019-12-05 08:16:25 -0500

    permute test added

--- permute_test_pim.py
+++ permute_test_pim.py
@@ -13,11 +13,6 @@ pim_df = pd.read_csv("./pim_vectors_20.csv")
 pim_df.gest = pim_df.gest.astype("category")
 pim_df = pim_df.loc[pim_df.gest != 1, :] # excl. well seperated gest 1
 
-### persistence image object ### might not need this
-#pimsd = 1e-5
-#px = 20
-#pim = PersImage(pixels=[px,px], spread=pimsd)
-
 
 ######### first compute accuracy score with properly labeled gestures ##########
 
@@ -30,9 +25,10 @@ log_reg = LogisticRegression(
     C=1e6,
     solver="newton-cg",
     fit_intercept=True,
-    max_iter=100,
+    max_iter=1000,
     multi_class="multinomial",
     random_state=1)
+
 
 s = 5 # number of CV folds
 skf = StratifiedKFold(n_splits = s, random_state = 1, shuffle = True)
@@ -45,22 +41,31 @@ for train_idx, test_idx in skf.split(pims, gests):
     fold_acc[f] = log_reg.score(pims[test_idx, ], gests[test_idx])
     f += 1
 
+fold_acc = fold_acc.reshape(1, s)
 
 ########## now compute accuracy score with randomly permuted labels ############
-perms = choice(np.arange(gests.size), size = gests.size, replace = False)
-perm_gests = gests[perms]
+np.random.seed(1)
 
-fold_acc_perm = np.zeros(s)
-f = 0
-for train_idx, test_idx in skf.split(pims, perm_gests):
-    print("Fold number " + str(f), end="\r")
-    log_reg.fit(pims[train_idx, ], perm_gests[train_idx])
-    fold_acc_perm[f] = log_reg.score(pims[test_idx, ], perm_gests[test_idx])
-    f += 1
+pitrs = 10 # number of permutation iterations
+fold_acc_perm = np.zeros((pitrs, s))
+
+for i in range(pitrs):
+    # generate a new permutation of labels
+    perms = choice(np.arange(gests.size), size = gests.size, replace = False)
+    perm_gests = gests[perms]
+
+    # perform permutation number p
+    f = 0
+    for train_idx, test_idx in skf.split(pims, perm_gests):
+        print("Permutation number " + str(i) + "; Fold number " + str(f)) #, end="\r")
+        log_reg.fit(pims[train_idx, ], perm_gests[train_idx])
+        fold_acc_perm[i, f] = log_reg.score(pims[test_idx, ], perm_gests[test_idx])
+        f += 1
 
 
-scores = np.r_[fold_acc, fold_acc_perm].reshape(2, -1)
-print(scores)
+scores = np.r_[fold_acc, fold_acc_perm].reshape(pitrs+1, -1)
 
-print("Avg Accuracy:")
-print(scores.mean(axis=1))
+avg_scores = scores.mean(axis=1)
+
+score_df = pd.DataFrame(avg_scores)
+score_df.to_csv("permutation_results.csv", index=False)
