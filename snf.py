d32fbbad3b8c60fb4ce5834ef824bfc4358f4665
Sam Voisin <psv6@duke.edu>
2020-01-16 15:07:11 -0500

    working on snf

--- snf.py
+++ snf.py
@@ -26,17 +26,45 @@ class SNF(SSM):
 
         Ref: Multiscale Geometric Summaries Similarity-based Sensor Fusion (p.4)
         """
-        self.trans_mat = np.zeros(shape=(self.n_mods, self.n_obs, self.n_obs))
+        self.trans_mtrx = np.zeros(shape=(self.n_mods, self.n_obs, self.n_obs))
         for m in range(self.n_mods): # loop over modalities
             for i in range(self.n_obs): # loop over rows in SSM
                 for j in range(self.n_obs): # loop over columns in SSM
                     if i != j:
-                        self.trans_mat[m,i,j] = (
+                        self.trans_mtrx[m,i,j] = (
                             self.array[m,i,j] /
                             (2*(self.array[m,i,:].sum()-self.array[m,i,i]))
                             )
                     else:
-                        self.trans_mat[m,i,j] = 0.5
+                        self.trans_mtrx[m,i,j] = 0.5
+
+
+    def calc_masked_transition_matrix(self, k):
+        """
+        calculate a "masked" transition probability matrix from the SSM. This
+        matrix represents a graph where vertices are data points and edges are
+        this distances between vertices
+
+        k - int; number of nearest neighbors to a given vertex i (i.e. those js
+        with the k largest similarity values in the ith row of the SSM)
+
+        NOTE: transition matrix must exist; run `self.calc_transition_matrix()`
+
+        Ref: Multiscale Geometric Summaries Similarity-based Sensor Fusion (p.4)
+        """
+        self.masked_mtrx = np.zeros(shape=(self.n_mods, self.n_obs, self.n_obs))
+        for m in range(self.n_mods): # loop over modalities
+            for i in range(self.n_obs): # loop over rows in SSM
+                k_sim_vals = self.array[m,i,:].copy()
+                k_sim_vals.sort()
+                k_sim_vals = k_sim_vals[-k:]
+                for j in range(self.n_obs): # loop over columns in SSM
+                    if i self.array[m,i,j] in k_sim_vals:
+                        self.masked_mtrx[m,i,j] = (
+                            self.array[m,i,j] /
+                            (2*k_sim_vals[-k:].sum())
+                            )
+
 
 
 
