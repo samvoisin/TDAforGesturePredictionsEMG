c8f3d6fa7ea440c89b8c9518d7a91dff1d0e0db2
Sam Voisin <psv6@duke.edu>
2020-03-19 12:37:50 -0400

    cleared print from snf

--- similarity_network_fusion.py
+++ similarity_network_fusion.py
@@ -204,7 +204,6 @@ class SNF(SSM):
         self.P_knn = np.zeros(shape=(self.n_mods, self.n_obs, self.n_obs))
         for m in range(self.n_mods): # loop over modalities
             for m in range(self.n_mods): # loop over modalities
-                print(self.W_knn[m,:,:].sum(axis=1).reshape(-1,1))
                 self.P_knn[m,:,:] = (
                     self.W_knn[m,:,:] /
                     self.W_knn[m,:,:].sum(axis=1).reshape(-1,1)
